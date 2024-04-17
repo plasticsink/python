cedc92f184493c98edecc7a5503053c49eb1796f
plasticsink <kaba.youka@gmail.com>
2024-04-17 02:35:45 +0000

    mysql client details

--- starting/settings.py
+++ starting/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'starting.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+
+    # }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'YouKaba$demo',  
+        'USER': 'YouKaba',  
+        'PASSWORD': 'django003',  
+        'HOST': 'YouKaba.mysql.pythonanywhere-services.com',  
+        'PORT': '3306',  
     }
+
 }
 
 
